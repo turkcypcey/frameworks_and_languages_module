13da7ad561bdbc0b6a68395be5b4dfce09a0ed31
turkcypcey <cc835@canterbury.ac.uk>
2021-12-13 16:38:23 +0000

    app.js updated [2]

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 <h1> Frameworks and Languages <h1/>
+
   <p> MCOMD3PFL <p/>
   <p> Ceyhan Cesur <p/>
+  <p> Allan Moderator <p/>
+
   
     
--- server/app.js
+++ server/app.js
@@ -14,9 +14,23 @@ ITEMS = {
   }
 }
 
+
 app.get('/', (req, res) => {
-  res.json({})
+  res.send('Hello World!')
+
+  // https://expressjs.com/en/4x/api.html#res.json
+  res.json({ hello: 'world' })
 })
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+// Docker container exit handler
+// https://github.com/nodejs/node/issues/4182
+process.on('SIGINT', function() {
+    process.exit();
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
